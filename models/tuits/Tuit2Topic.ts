4791c99408412614730ed66ee645084aaf70c676
laimax17 <caicai0660@gmail.com>
2022-02-03 21:10:46 -0500

    modified

---
+++ models/tuits/Tuit2Topic.ts
@@ -0,0 +1,6 @@
+import Tuit from "../tuits/Tuit";
+
+export default class Tuit2Topic {
+    private topic: String = '';
+    private tuit: Tuit | null = null;;
+}
--- server.ts
+++ server.ts
@@ -1,8 +1,30 @@
-import express from 'express';
-const app = express();
+/**
+ * @file Server file
+ */
+ import express, {Request, Response} from 'express';
+ import mongoose from "mongoose";
 
-app.get('/hello', (req, res) =>
-    res.send('Hello World!'));
+ const mongoUrl = "mongodb+srv://max17:max17@cluster0.gci7e.mongodb.net/tuiter?retryWrites=true&w=majority"
 
-const PORT = 4000;
-app.listen(process.env.PORT || PORT);
+ mongoose.connect(mongoUrl);
+ import bodyParser from "body-parser";
+ //import CourseController from "./controllers/CourseController";
+ import UserController from "./controllers/UserController";
+ import TuitController from "./controllers/TuitController";
+ //import LikeController from "./controllers/LikeController";
+ const app = express();
+ app.use(bodyParser.json())
+ 
+ app.get('/', (req: Request, res: Response) =>
+     res.send('Welcome!'));
+ 
+ app.get('/add/:a/:b', (req: Request, res: Response) =>
+     res.send(req.params.a + req.params.b));
+ 
+ //const courseController = new CourseController(app);
+ const userController = UserController.getInstance(app);
+ const tuitController = TuitController.getInstance(app);
+ //const likesController = LikeController.getInstance(app);
+ 
+ const PORT = 4000;
+ app.listen(process.env.PORT || PORT);
