d9a33bbd06ea5b080c2e23c763e631e767a9137a
laimax17 <caicai0660@gmail.com>
2022-02-21 17:32:16 -0500

    edit server.ts file

--- server.ts
+++ server.ts
@@ -9,14 +9,18 @@
 
  const db_username = process.env.DB_USERNAME;
  const db_password = process.env.DB_PASSWORD;
- const mongoUrl = `mongodb+srv://${db_username}:${db_password}@cluster0.gci7e.mongodb.net/tuiter?retryWrites=true&w=majority`;
+ const mongoUrl = `mongodb+srv://max17:max17@cluster0.gci7e.mongodb.net/tuiter?retryWrites=true&w=majority`;
 
  mongoose.connect(mongoUrl);
  import bodyParser from "body-parser";
  //import CourseController from "./controllers/CourseController";
  import UserController from "./controllers/UserController";
  import TuitController from "./controllers/TuitController";
- //import LikeController from "./controllers/LikeController";
+ import LikeController from "./controllers/LikeController";
+ import BookmarkController from "./controllers/BookmarkController";
+ import MessageController from "./controllers/MessageController";
+ import FollowController from "./controllers/FollowController";
+
  const app = express();
  app.use(bodyParser.json())
  
@@ -29,7 +33,10 @@
  //const courseController = new CourseController(app);
  const userController = UserController.getInstance(app);
  const tuitController = TuitController.getInstance(app);
- //const likesController = LikeController.getInstance(app);
+ const likesController = LikeController.getInstance(app);
+ const bookmarkController = BookmarkController.getInstance(app);
+ const messageController = MessageController.getInstance(app);
+ const followController = FollowController.getInstance(app);
  
  const PORT = 4000;
  app.listen(process.env.PORT || PORT);
