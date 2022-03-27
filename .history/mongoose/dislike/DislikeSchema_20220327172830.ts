22a19ae4d9af3ff782ef3135d3c180574a58fc2b
laimax17 <caicai0660@gmail.com>
2022-03-27 17:32:09 -0400

    dislike model and schema

--- .history/mongoose/dislike/DislikeSchema_20220327172830.ts
+++ .history/mongoose/dislike/DislikeSchema_20220327172830.ts
@@ -1,8 +1,8 @@
 import mongoose, {Schema} from "mongoose";
 import Dislike from "../../models/dislikes/Dislike";
 
-const DislikeSchema = new mongoose.Schema<Dislike>({
+const DisLikeSchema = new mongoose.Schema<Dislike>({
     tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
     dislikedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
 }, {collection: "dislikes"});
-export default DislikeSchema;
+export default DisLikeSchema;
